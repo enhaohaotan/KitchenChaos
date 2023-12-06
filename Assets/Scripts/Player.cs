f1dd59e1c06477f80c3a0c29953096f725ce60c0
Enhao Tan <enhaohao.tan@gmail.com>
2023-12-06 23:31:54 +0100

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField] private float moveSpeed = 7f;
+
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -15,14 +17,16 @@ public class Player : MonoBehaviour
             inputVector.y = -1;
         }
         if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = +1;
+            inputVector.x = -1;
         }
         if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = -1;
+            inputVector.x = +1;
         }
 
         inputVector = inputVector.normalized;
 
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
         Debug.Log(inputVector);
     }
 
