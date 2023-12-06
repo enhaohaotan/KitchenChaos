78df11eb5f632d8ba49d6ebddb41dbb4f3810702
Enhao Tan <enhaohao.tan@gmail.com>
2023-12-06 23:13:41 +0100

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private void Update() {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x = +1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector.x = -1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Debug.Log(inputVector);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
