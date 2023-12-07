28b3033cd7585490a5f3a07899df3e65690d81d1
Enhao Tan <enhaohao.tan@gmail.com>
2023-12-07 23:36:39 +0100

    fully control the player

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private const string IS_WALKLING = "IsWalking";
+
+    [SerializeField] private Player player;
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        animator.SetBool(IS_WALKLING, player.IsWalking());
+    }
+}
